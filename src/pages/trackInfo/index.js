cf6784d2a80c3b441dc19e8834a01e95da4ee7e5
Abdo <abdopoetry@gmail.com>
2019-10-18 11:25:13 +0200

    Integrate ESlint

--- src/pages/trackInfo/index.js
+++ src/pages/trackInfo/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const TrackInfo = () => {
-  return (
-    <div>
-      <h3>TrackInfo</h3>
-    </div>
-  );
-};
+const TrackInfo = () => (
+  <div>
+    <h3>TrackInfo</h3>
+  </div>
+);
 
 export default TrackInfo;
